a6d27facdf983ebbd3fef09e0ef4aa3b208989c2
Tiago dos Santos Suleiman Moreira <tsuleiman@prefeitura.sp.gov.br>
2020-03-12 12:35:39 -0300

    cross origin

--- src/main/java/com/suleiman/contato/resource/ContatosResource.java
+++ src/main/java/com/suleiman/contato/resource/ContatosResource.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.suleiman.contato.model.Contato;
 import com.suleiman.contato.repository.Contatos;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/contatos")
 public class ContatosResource {
